4ccee023daccdf367f17752dd013592374a69c8b
zoeleeee <zhuzby@outlook.com>
2020-04-05 23:47:52 +0800

    .

--- lagrange_parameters.py
+++ lagrange_parameters.py
@@ -5,19 +5,19 @@ import operator
 mpf.dps = 100
 n = 256
 
-x = np.arange(n)
-y = np.load('permutation/256_256.1_permutation.npy')
+x = range(n)
+y = list(np.load('permutation/256_256.1_permutation.npy'))
 
 xx = np.array([mpf(val) for val in x]) / 255.
-yy = np.array([mpf(val) for val in y]) / 255.
+yy = np.array([mpf(str(val)) for val in y]) / 255.
 
 coef, s = [], []
 #256*256
 coef.append([yy[i] / reduce(operator.mul, [xx[i]-xx[j] if i!=j else 1 for j in range(len(xx))]) for i in range(len(xx))])
 s.append(mpf(sum(xx)))
-coef.append(-[s[0] - xx[i] for i in range(n)])
+coef.append(-1*[s[0] - xx[i] for i in range(n)])
 s.append(mpf(sum([s[0]*xx[j]-xx[j]**2 for j in range(n)]))/2)
-coef.append([s[1] - xx[i]*coef[1] for i in range(n)])
+coef.append([s[1] - xx[i]*coef[1][i] for i in range(n)])
 
 for i in range(3, n):
 	sign = 1 if i%2==0 else -1
@@ -25,7 +25,7 @@ for i in range(3, n):
 	coef.append(sign*[s[i-1]-xx[j]*coef[i-1][j] for j in range(n)])
 
 coef = np.array(coef)
-param = [sum(coef[i] * coef[0]) if i！=0 else sum(coef[0]) for i in range(len(coef))]
+param = [sum(coef[i] * coef[0]) if i!=0 else sum(coef[0]) for i in range(len(coef))]
 print(param)
 
 for j, v in enumerate(xx):
