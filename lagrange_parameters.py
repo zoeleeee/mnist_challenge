e25fdbafa093db0345e84ad85144e0cee438195d
zoeleeee <zhuzby@outlook.com>
2020-04-05 23:34:28 +0800

    .

--- lagrange_parameters.py
+++ lagrange_parameters.py
@@ -14,19 +14,23 @@ yy = np.array([mpf(val) for val in y]) / 255.
 coef, s = [], []
 #256*256
 coef.append([yy[i] / reduce(operator.mul, [xx[i]-xx[j] if i!=j else 1 for j in range(len(xx))]) for i in range(len(xx))])
-s.append(mpf(sum(x)))
-coef.append(-[s[0] - i for i in range(n)])
-s.append(mpf(sum([s[0]*j-j**2 for j in range(n)]))/2)
-coef.append([s[1] - i*coef[1] for i in range(n)])
+s.append(mpf(sum(xx)))
+coef.append(-[s[0] - xx[i] for i in range(n)])
+s.append(mpf(sum([s[0]*xx[j]-xx[j]**2 for j in range(n)]))/2)
+coef.append([s[1] - xx[i]*coef[1] for i in range(n)])
 
 for i in range(3, n):
 	sign = 1 if i%2==0 else -1
-	s.append(mpf(sum([s[i-2]*j-(j**2)*coef[i-2][j] for j in range(n)]))/i)
-	coef.append(sign*[s[i-1]-j*coef[i-1][j] for j in range(n)])
+	s.append(mpf(sum([s[i-2]*xx[j]-(xx[j]**2)*coef[i-2][j] for j in range(n)]))/i)
+	coef.append(sign*[s[i-1]-xx[j]*coef[i-1][j] for j in range(n)])
 
 coef = np.array(coef)
 param = [sum(coef[i] * coef[0]) if i！=0 else sum(coef[0]) for i in range(len(coef))]
 print(param)
+
+for j, v in enumerate(xx):
+	tmp = sum([(v**i)*param[n-i-1] for i in range(n)])
+	print(tmp==y[j], tmp, yy[j])
 
 ## TEST
 # def parameters(n):
