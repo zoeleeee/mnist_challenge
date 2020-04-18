8ff8b287e74d189767151315ecc0d2d84243a9f3
zoeleeee <zhuzby@outlook.com>
2020-04-18 15:26:34 +0800

    .

---
+++ hsja_attack.py
@@ -0,0 +1,75 @@
+import json
+import tensorflow as tf 
+from tensorflow import keras
+import numpy as np
+
+with open('models/mnist.json') as file:
+    json_model = file.read()
+
+model = keras.models.model_from_json(json_model)
+model.load_weights('models/mnist.h5')
+
+x_val = np.load('data/mnist_data.npy')[60000:].transpose((0,2,3,1)).astype(np.float32) / 255.
+labels = np.load('data/mnist_labels.npy')[60000:]
+
+from cleverhans.attacks import HopSkipJumpAttack
+from cleverhans.utils_keras import KerasModelWrapper
+keras.backend.set_learning_phase(0)
+sess = keras.backend.get_session()
+
+attack = MomentumIterativeMethod(HopSkipJumpAttack(model), sess=sess)
+bapp_params = {
+        'constraint': 'l2',
+        'stepsize_search': 'geometric_progression',
+        'num_iterations': 10,
+        'verbose': True,
+    }
+x_adv = self.attack.generate_np(x_val, **bapp_params)
+orig_labs = np.argmax(model.predict(x_val), axis=1)
+new_labs = np.argmax(model.predict(x_adv), axis=1)
+print(np.max(np.absolute(x_adv-x_val)))
+print('normal mnist model acc:', np.mean(orig_labs==labels))
+print('advs mnist model acc:', np.mean(new_labs==labels))
+print('advs acc:', new_labs[orig_labs==labels] != labels[orig_labs==labels])
+
+# from hop_skip_jump_attack import HopSkipJumpAttack
+# import json
+# import tensorflow as tf 
+# from tensorflow import keras
+# import numpy as np
+# from cleverhans.utils_keras import KerasModelWrapper
+# from utils import 
+
+# attack = HopSkipJumpAttack(KerasModelWrapper(model), sess)
+# x_adv = np.load('data/mnist_data.npy').transpose((0,2,3,1))
+
+# bapp_params = {
+#         'constraint': 'l2',
+#         'stepsize_search': 'geometric_progression',
+#         'num_iterations': 10,
+#         'verbose': True,
+#     }
+# x_adv = self.attack.generate_np(x_val, **bapp_params)
+
+# bapp_params = {
+#         'constraint': 'linf',
+#         'stepsize_search': 'grid_search',
+#         'num_iterations': 10,
+#         'verbose': True,
+#     }
+# bapp_params = {
+#         'constraint': 'linf',
+#         'stepsize_search': 'geometric_progression',
+#         'num_iterations': 10,
+#         'verbose': True,
+#         'y_target': y_target,
+#         'image_target': image_target,
+#     }
+# bapp_params = {
+#         'constraint': 'l2',
+#         'stepsize_search': 'grid_search',
+#         'num_iterations': 10,
+#         'verbose': True,
+#         'y_target': y_target_ph,
+#         'image_target': image_target_ph,
+#     }
