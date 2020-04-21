44e40e084023004cd65c9499e9706a5f6cc1b496
zoeleeee <zhuzby@outlook.com>
2020-04-21 09:09:50 +0800

    .

--- plot_lagrange.py
+++ plot_lagrange.py
@@ -7,8 +7,8 @@ file = sys.argv[-1]
 nb_values = int(sys.argv[-2])
 
 mp.dps = 5000
-a = np.load('lagrange/lag_'+file.split('/')[1], params) [0]
-b = np.load('lagrange/lag_iter_'+file.split('/')[1], res)[0]
+a = np.load('lagrange/lag_'+file.split('/')[1]) [0]
+b = np.load('lagrange/lag_iter_'+file.split('/')[1])[0]
 
 x = np.arange(nb_values)/nb_values
 xx = np.arange(255)/255.
@@ -19,14 +19,14 @@ yyxx = np.polyval(b, xx)
 
 plt.plot(x, y, color='plum')
 plt.scatter(xx, yx, color='lavender')
-plt.xticks(np.arange(0,1,1/255))
-plt.yticks(np.arange(0,1,1/255))
+plt.xticks(np.arange(0,1,15/255))
+plt.yticks(np.arange(0,1,15/255))
 plt.grid(linestyle='-.')
 plt.savefig('lagrange/plot_lagrange.png')
 
 plt.plot(x, yy, color='plum')
 plt.scatter(xx, yyxx, color='lavender')
-plt.xticks(np.arange(0,1,1/255))
-plt.yticks(np.arange(0,1,1/255))
+plt.xticks(np.arange(0,1,15/255))
+plt.yticks(np.arange(0,1,15/255))
 plt.grid(linestyle='-.')
 plt.savefig('lagrange/plot_lagrange_gradiants.png')
