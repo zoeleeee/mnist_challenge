3cb55bc974c760ab993e0f38507bbd28d4e319f0
zoeleeee <zhuzby@outlook.com>
2020-04-22 19:43:52 +0800

    .

--- multi_lagrange_iter_pgd_attack.py
+++ multi_lagrange_iter_pgd_attack.py
@@ -78,7 +78,7 @@ class LinfPGDAttack:
   def perturb(self, x_nat, y, org_img, sess, order, targeted=False):
     """Given a set of examples (x_nat, y), returns a set of adversarial
        examples within epsilon of x_nat in l_infinity norm."""
-    # tmp = copy.deepcopy(org_img.astype(np.float32))
+    tmp = copy.deepcopy(org_img.astype(np.float32))
     # print(tmp.shape)
     if self.rand:
       x = x_nat + np.random.uniform(-self.epsilon, self.epsilon, x_nat.shape)
@@ -95,17 +95,17 @@ class LinfPGDAttack:
 #      print(grad.shape, tmp.shape)
       sub_grad = np.array([np.polyval(self.param[j], [[[mpf(v[j]) for v in b] for b in a] for a in x]) for j in range(x.shape[-1])]).transpose((1,2,3,0))
       print(sub_grad.shape, grad.shape)
-      grad = np.sum(sub_grad*grad, axis=-1).reshape(x.shape)
+      grad = np.sum(sub_grad*grad, axis=-1)
       # print(np.array(grad).shape, np.sum(np.sign(grad)==0), np.sum(np.sign(grad)>0))
       if targeted:
-        x -= self.a*np.sign(grad).astype(np.float32)
+        tmp -= self.a*np.sign(grad).astype(np.float32)
       else:
-        x += self.a*np.sign(grad).astype(np.float32)
-      x = np.clip(x, x-self.epsilon, x+self.epsilon)
-      x = np.clip(x, 0, 1)
-      # tmp = np.clip(tmp, org_img - self.epsilon, org_img + self.epsilon)
-      # tmp = np.clip(tmp, 0, 1) # ensure valid pixel range
-      # x = np.array([[[order[int(v[0])] for v in b] for b in a] for a in tmp])
+        tmp += self.a*np.sign(grad).astype(np.float32)
+
+      tmp = np.clip(tmp, org_img - self.epsilon, org_img + self.epsilon)
+      tmp = (np.clip(tmp, 0, 1)*255).astype(np.int)
+      x = np.array([[[order[int(v[0])] for v in b] for b in a] for a in tmp])
+      tmp /= 255.
     # tmp = [[[[mpf(int(reduce(operator.add, [bin(int(v*255.))[2:].zfill(8) for v in c]), 2))] for c in b] for b in a] for a in x]
     # print(np.array(tmp).shape)
     # tmp = [[[[int(np.polyval(self.param, v)) for v in c] for c in b] for b in a] for a in tmp]
--- plot_lagrange.py
+++ plot_lagrange.py
@@ -7,35 +7,38 @@ file = sys.argv[-1]
 nb_values = int(sys.argv[-2])
 
 mp.dps = 1000
-a = np.load('lagrange/lag_'+file.split('/')[1]) [0]
-b = np.load('lagrange/lag_iter_'+file.split('/')[1])[0]
+aa = np.load('lagrange/lag_'+file.split('/')[1])
+bb = np.load('lagrange/lag_iter_'+file.split('/')[1])
 
-x = np.array([mpf(val) for val in range(nb_values)])/nb_values
-xx = np.array([mpf(val) for val in range(256)])/255.
-yy = np.polyval(b, x)
-yyxx = np.polyval(b, xx)
-print(len(yyxx), [float(v) for v in yyxx])
-y = np.polyval(a, x)
-yx = np.polyval(a, xx)
-print(len(yx), [int(v*255) for v in yx])
+for i in range(a.shape[0]):
+	a, b = aa[i], bb[i]
 
+	x = np.array([mpf(val) for val in range(nb_values)])/nb_values
+	xx = np.array([mpf(val) for val in range(256)])/255.
+	yy = np.polyval(b, x)
+	yyxx = np.polyval(b, xx)
+	print(len(yyxx), [float(v) for v in yyxx])
+	y = np.polyval(a, x)
+	yx = np.polyval(a, xx)
+	print(len(yx), [int(v*255) for v in yx])
 
-plt.ylim(-1,2)
-plt.plot(x, y, color='plum', lw=.3)
-plt.scatter(xx, yx, color='indigo', s=.5, alpha=1)
-plt.xticks(np.arange(0,1.0001,15/255), ['0/255', '15/255', '30/255', '45/255', '60/255', '75/255', '90/255', '105/255', '120/255', '135/255', '150/255', '165/255', '180/255', '195/255', '210/255', '225/255', '240/255', '255/255'], fontsize='x-small', rotation=270)
-plt.yticks(np.arange(0,1.0001,15/255), ['0/255', '15/255', '30/255', '45/255', '60/255', '75/255', '90/255', '105/255', '120/255', '135/255', '150/255', '165/255', '180/255', '195/255', '210/255', '225/255', '240/255', '255/255'], fontsize='x-small')
+	plt.clf()
+	plt.ylim(-1,2)
+	plt.plot(x, y, color='plum', lw=.3)
+	plt.scatter(xx, yx, color='indigo', s=.5, alpha=1)
+	plt.xticks(np.arange(0,1.0001,15/255), ['0/255', '15/255', '30/255', '45/255', '60/255', '75/255', '90/255', '105/255', '120/255', '135/255', '150/255', '165/255', '180/255', '195/255', '210/255', '225/255', '240/255', '255/255'], fontsize='x-small', rotation=270)
+	plt.yticks(np.arange(0,1.0001,15/255), ['0/255', '15/255', '30/255', '45/255', '60/255', '75/255', '90/255', '105/255', '120/255', '135/255', '150/255', '165/255', '180/255', '195/255', '210/255', '225/255', '240/255', '255/255'], fontsize='x-small')
 
-plt.grid(linestyle='-')
-plt.savefig('lagrange/plot_lagrange.png')
-plt.clf()
+	plt.grid(linestyle='-')
+	plt.savefig('lagrange/lagrange_16_{}.png'.format(i))
+	plt.clf()
 
-#plt.ylim(-1e5,1e5)
-plt.yscale('symlog')
-plt.plot(x, yy, color='plum', lw=.3)
-plt.scatter(xx, [float(v) for v in yyxx], color='indigo', s=.5, alpha=1)
-plt.xticks(np.arange(0,1.0001,15/255), ['0/255', '15/255', '30/255', '45/255', '60/255', '75/255', '90/255', '105/255', '120/255', '135/255', '150/255', '165/255', '180/255', '195/255', '210/255', '225/255', '240/255', '255/255'], fontsize='x-small', rotation=270)
-plt.yticks(fontsize='x-small')
-# plt.yticks(np.arange(0,1,15/255), ['15/255', '30/255', '45/255', '60/255', '75/255', '90/255', '105/255', '120/255', '135/255', '150/255', '165/255', '180/255', '195/255', '210/255', '225/255', '240/255'])
-plt.grid(axis='x', linestyle='-')
-plt.savefig('lagrange/plot_lagrange_gradiants.png')
+	#plt.ylim(-1e5,1e5)
+	plt.yscale('symlog')
+	plt.plot(x, yy, color='plum', lw=.3)
+	plt.scatter(xx, [float(v) for v in yyxx], color='indigo', s=.5, alpha=1)
+	plt.xticks(np.arange(0,1.0001,15/255), ['0/255', '15/255', '30/255', '45/255', '60/255', '75/255', '90/255', '105/255', '120/255', '135/255', '150/255', '165/255', '180/255', '195/255', '210/255', '225/255', '240/255', '255/255'], fontsize='x-small', rotation=270)
+	plt.yticks(fontsize='x-small')
+	# plt.yticks(np.arange(0,1,15/255), ['15/255', '30/255', '45/255', '60/255', '75/255', '90/255', '105/255', '120/255', '135/255', '150/255', '165/255', '180/255', '195/255', '210/255', '225/255', '240/255'])
+	plt.grid(axis='x', linestyle='-')
+	plt.savefig('lagrange/lagrange_gradiants_16_{}.png'.format(i))
