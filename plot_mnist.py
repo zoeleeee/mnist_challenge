726212df8a3842defc4892171ade5cd62011a70f
zoeleeee <zhuzby@outlook.com>
2020-04-30 20:03:04 +0800

    .

--- plot_mnist.py
+++ plot_mnist.py
@@ -7,5 +7,5 @@ label = int(sys.argv[-2])
 idx = file.split('_')[-2]
 imgs = np.load(file)
 for i, img in enumerate(imgs):
-	plt.imshow(img)
-	plt.savefig("mnist_pics/fig#{}_{}.png".format(idx, (label+i)%10), cmap='Greys')
+    plt.imshow(img)
+    plt.savefig("mnist_pics/fig#{}_{}.png".format(idx, (label+i)%10), cmap='Greys')
