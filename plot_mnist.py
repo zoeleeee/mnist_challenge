ed553b58e01eafe875ea3837a24651ef444fe1d9
zoeleeee <zhuzby@outlook.com>
2020-04-30 19:54:27 +0800

    .

---
+++ plot_mnist.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+
+file = sys.argv[-1]
+label = int(sys.argv[-2])
+idx = file.split('_')[-2]
+imgs = np.load(file)
+for i, img in enumerate(imgs):
+	plt.imshow(img)
+	plt.savefig("mnist_pics/fig#{}_{}.png".format(idx, (label+i)%10), cmap='Greys')
