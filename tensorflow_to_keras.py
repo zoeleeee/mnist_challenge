5d87daff7d4224ba3f1e21cbb9df4221b4615ad1
zoeleeee <zhuzby@outlook.com>
2020-05-06 17:18:08 +0800

    .

---
+++ tensorflow_to_keras.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+import keras
+import sys
+
+model_dir = sys.argv[-1]
+checkpoint = tf.train.latest_checkpoint(model_dir)
+with tf.Session() as sess:
+    # load weights for graph
+    saver.restore(sess, checkpoint)
+
+    # get all global variables (including model variables)
+    vars_global = tf.global_variables()
+
+    # get their name and value and put them into dictionary
+    sess.as_default()
+    model_vars = {}
+    for var in vars_global:
+        try:
+            model_vars[var.name] = var.eval()
+        except:
+            print("For var={}, an exception occurred".format(var.name))
+    print(model_vars.keys())
