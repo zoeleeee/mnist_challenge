5ca0653abdc867f28276c62568d4239ec772f2b8
harry <yulong.t.nju@gmail.com>
2020-05-11 16:35:29 +0800

    .

--- keras_train.py
+++ keras_train.py
@@ -20,8 +20,9 @@ model_dir = config['model_dir']
 nb_labels = config['num_labels']
 path = config['permutation']
 st_lab = config['start_label']
-np.random.seed(st_lab)
+#np.random.seed(st_lab)
 lab_perm = np.random.permutation(np.load('2_label_permutation.npy')[:nb_labels].T)#[st_lab:st_lab+nb_labels].T)
+lab_perm = np.load('2_label_permutation.npy')[st_lab:st_lab+nb_labels].T
 
 # Setting up training parameters
 tf.set_random_seed(config['random_seed'])
