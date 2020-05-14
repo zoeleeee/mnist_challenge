cf4956d8b4d81ea7e32932f7525ffdbe8ec5f6ce
harry <yulong.t.nju@gmail.com>
2020-05-14 18:12:52 +0800

    .

--- keras_multi_eval.py
+++ keras_multi_eval.py
@@ -39,8 +39,8 @@ nb_channal = int(config['permutation'].split('_')[1].split('.')[1])
 # imgs = order_extend_data(perm, imgs)
 # labels = np.load('data/mnist_labels.npy')
 # input_shape = imgs.shape[1:]
-imgs, labels, input_shape, model_dir = two_pixel_perm_img(nb_channal, model_dir)
-# imgs, labels, input_shape, model_dir = two_pixel_perm_sliding(nb_channal, model_dir)
+imgs, labels, input_shape, model_dir = two_pixel_perm(nb_channal, model_dir)
+#imgs, labels, input_shape, model_dir = two_pixel_perm_sliding(nb_channal, model_dir)
 # imgs, labels, input_shape = load_data(config['permutation'], config['num_labels'])
 labels = np.array([rep[i] for i in labels]).astype(np.float32)
 #x_train, y_train = imgs[:60000], labels[:60000]
@@ -49,8 +49,8 @@ if dataset != 'origin.npy':
   x_test = np.load(dataset)
   if dataset.endswith('show.npy'):
     # x_test = extend_data(config['permutation'], x_test)
-    x_test = two_pixel_perm(nb_channal, x_test)
-    # x_test = two_pixel_perm_sliding_img(nb_channal, x_test)
+    x_test = two_pixel_perm_img(nb_channal, x_test)
+#    x_test = two_pixel_perm_sliding_img(nb_channal, x_test)
     # x_test = order_extend_data(perm, x_test)
 
 if len(x_test.shape) == 3:
