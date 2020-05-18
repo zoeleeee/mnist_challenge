9866b36706828a11fa0255d878b447330606eb94
zoeleeee <zhuzby@outlook.com>
2020-05-18 16:53:34 +0800

    .

--- KitModel_sequential.py
+++ KitModel_sequential.py
@@ -8,15 +8,16 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         self.conv1 = nn.Conv2d(1, 32, 5, padding=2)
-        self.pool = nn.MaxPool2d(2, 2)
+        self.pool1 = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(32, 64, 5, padding=2)
+        self.pool2 = nn.MaxPool2d(2, 2)
         self.fc1 = nn.Linear(3136, 1024)
         self.fc2 = nn.Linear(1024, 10)
         # self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool1(F.relu(self.conv1(x)))
+        x = self.pool2(F.relu(self.conv2(x)))
         x = x.view(-1, 1024)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
