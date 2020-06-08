ac9ebe72478f256557f1455c67f40b6bfb2b9aac
zoeleeee <zhuzby@outlook.com>
2020-06-08 16:30:19 +0800

    .

--- permutation.py
+++ permutation.py
@@ -8,7 +8,7 @@ for t in range(16):
     perms = []
     for j in range(256*256*256):
         perms.append(np.random.permutation(np.arange(256)))
-    tmp = np.array([[[perms[a[i-1][j-1]][a[i-1][j]][a[i][j-1]][a[i][j]] for i in range(1, len(a[j]), 1)] for j in range(1, len(a), 1)] for a in imgs])
+    tmp = np.array([[[perms[a[i-1][j-1]][a[i-1][j]][a[i][j-1]][a[i][j]] for j in range(1, len(a[i]), 1)] for i in range(1, len(a), 1)] for a in imgs])
     print(tmp.size())
     new_data.append(tmp)
 np.save('data/window_mnist_data.npy', np.arrray(new_data).transpose((1,2,3,0)))
