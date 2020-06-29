3d4b4473aeecd0dec1bad719c9da7228ae5f09f5
zoeleeee <zhuzby@outlook.com>
2020-06-29 11:27:23 +0800

    .

--- pytorch_blackbox_attack.py
+++ pytorch_blackbox_attack.py
@@ -109,6 +109,7 @@ def attack_targeted(model, train_dataset, x0, y0, target, alpha = 0.1, beta = 0.
         if predict(model,xi) == target:
             theta = torch.tensor(xi) - x0
             initial_lbd = torch.norm(theta)
+            print(theta, torch.norm(theta))
             theta = theta/torch.norm(theta)
             lbd, count = fine_grained_binary_search_targeted(model, x0, y0, target, theta, initial_lbd)
             query_count += count
